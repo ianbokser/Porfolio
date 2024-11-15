9d56a8ee9b72f4c29d851c0128bd4688016d2e1a
chuly <chulygames@hotmail.com>
2024-11-14 23:31:56 -0300

    visual

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Logo } from '../Logo/Logo';
 import { Link } from 'react-scroll';
+import './Header.css';
 
 export const Header = () => {
   return (
-    <header className="bg-gray-800/80 shadow-md backdrop-blur-md rounded-b-lg">
-      <div className="w-full max-w-screen-lg mx-auto flex flex-col justify-between items-center py-2">
+    <header className="bg-gray-800/80 shadow-md backdrop-blur-md rounded-b-lg w-full">
+      <div className="w-full max-w-screen-lg mx-auto flex flex-col items-center py-2">
         <Logo />
-        <nav className="flex space-x-6">
+        <nav className="flex space-x-6 justify-center w-full mt-2">
           {['Sobre Mí', 'Proyectos', 'Skills', 'Contacto'].map((section) => (
             <Link
               key={section}
